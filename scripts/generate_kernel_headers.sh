fe59561e39e5579d10038775daa13a6759bec4f9
Ahmed S. Darwish <darwi@linutronix.de>
2023-12-11 00:02:24 +0100

    scripts: Introduce generate_kernel_headers.sh
    Add a script for generating the fule range of CPUID linux kernel headers,
    and collect them at an "output/" folder.
    
    The script should be helpful for testing and CI/CD pipelines at a later
    stage, as it touches and transforms all of the project's XML leaf files.
    
    Signed-off-by: Ahmed S. Darwish <darwi@linutronix.de>

---
+++ scripts/generate_kernel_headers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# generate_kernel_headers.sh — Linux kernel CPUID headers generation
+#
+# SPDX-FileCopyrightText: 2023 Linutronix GmbH
+# SPDX-License-Identifier: GPL-2.0-only
+
+set -o errtrace
+set -o nounset
+set -o pipefail
+set -o errexit
+
+OUTPUT_DIR=output/
+
+mkdir -p $OUTPUT_DIR
+
+for file in db/xml/leaf_*.xml; do
+    num=${file#db/xml/leaf_}
+    num=${num%.xml}
+
+    echo "Generating kernel C header for CPUID leaf 0x${num}"
+    poetry run cpuidgen --kheader $num > ${OUTPUT_DIR}/leaf_${num}.h
+done
